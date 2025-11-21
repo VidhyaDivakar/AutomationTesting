9444e479ba56b6e868aebe577c8240a1aa2d3384
Vidhya Divakar <vidhyaminuteread@gmail.com>
2025-11-21 09:23:42 -0500

    Updated the FibonacciSeriesWIHA.java

---
+++ src/main/java/week1day2/FibonacciSeriesW1HA.java
@@ -0,0 +1,23 @@
+// A simple Java program to print the Fibonacci series up to 7 terms
+package src.main.java.week1day2;
+
+//public is the access modifier. It allows the class to be accessible from other classes.
+public class FibonacciSeriesW1HA {
+    /**
+     * The main method is the entry point of any Java program. Static means it can
+     * be called without creating an instance of the class.
+     * args is an array of strings that stores command-line arguments. Here, it is
+     * not used.
+     */
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        for (int i = 0; i <= 7; i++) {
+            System.out.println(a);
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+    }
+}
